ba454da25b0d31943fff5aa96fbc715f72de86ba
maxkuhn <43338350+maxkuhn@users.noreply.github.com>
2023-12-04 22:27:18 +0100

    add working validation for puzzle 1, day 3

--- 2023/day_03_puzzle_1.py
+++ 2023/day_03_puzzle_1.py
@@ -1,6 +1,7 @@
 """Advent of code, 03.12.2023, puzzle 1"""
 
 import os
+from typing import Iterable
 
 import numpy as np
 
@@ -53,24 +54,25 @@ def generate_integer_and_symbol_arrays(input_array: np.array) -> (np.array, np.a
 def main(input_array: np.array):
     """TODO"""
     integer_array, symbol_array = generate_integer_and_symbol_arrays(input_array)
+    integer_coordinates = []
+    integers_found = []
     for line_number, array in enumerate(integer_array):
-        print(array)
+        # print(array)
         # get coordinates of integers
         number_fields = []
         for field_number, field in enumerate(array):
             if all((not np.isnan(field), field >= 0)):
                 number_fields.append(field_number)
-        print(number_fields)
+        # print(number_fields)
         # skip if no numbers are found
         if not number_fields:
             continue
         # get coordinates of numbers
-        numbers_found = []
         locations_found = []
         current_location = 0
         number_as_string = ""
         for i, number in enumerate(number_fields):
-            print(number, current_location)
+            # print(number, current_location)
             if i == 0:
                 number_as_string = str(int(array[number]))
                 locations_found = [[number]]
@@ -78,13 +80,60 @@ def main(input_array: np.array):
                 number_as_string += str(int(array[number]))
                 locations_found[-1].append(number)
             elif number > current_location + 1:  # not adjacent
-                numbers_found.append(int(number_as_string))
+                integers_found.append(int(number_as_string))
                 locations_found.append([number])
                 number_as_string = str(int(array[number]))
             current_location = number
-        numbers_found.append(int(number_as_string))
-        print(locations_found, numbers_found)
-        # check whether symbol adjacent
+        integers_found.append(int(number_as_string))
+        print(locations_found, integers_found)
+        integer_coordinates.append([line_number, locations_found])
+    print(integer_coordinates)
+    # check whether symbol adjacent
+    adjacent_symbol_check = []
+    print(symbol_array)
+    for coordinates in integer_coordinates:
+        line, row_list = coordinates
+        for rows in row_list:
+            print(line, rows)
+            lines_to_search = range(max(line-1, 0), min(line+2, integer_array.shape[0]))
+            rows_to_search = range(max(rows[0]-1, 0), min(rows[-1]+2, integer_array.shape[0]))
+            print(lines_to_search, rows_to_search)
+            adjacent_symbol_check.append(
+                symbol_at_integer_location(
+                    symbol_array, lines_to_search, rows_to_search, line, rows))
+    print(adjacent_symbol_check)
+    print(integers_found)
+    relevant_integers = get_relevant_integers(integers_found, adjacent_symbol_check)
+    print(relevant_integers)
+    assert relevant_integers == example_output
+
+
+def symbol_at_integer_location(
+        symbol_array: np.array,
+        lines_to_search: Iterable,
+        rows_to_search: Iterable,
+        line_of_integer: int,
+        rows_of_integer: list[int],
+) -> bool:
+    """Check whether a symbol is present next to the integer."""
+    for line in lines_to_search:
+        for row in rows_to_search:
+            print(line, row, symbol_array[line, row], symbol_array[line, row] == 1)
+            if line == line_of_integer and row in rows_of_integer:
+                continue
+            elif symbol_array[line, row] == 1.0:
+                print("True found", line, row)
+                return True
+    print("Not found")
+    return False
+
+
+def get_relevant_integers(integers_list: list[int], symbols_check: list[bool]) -> list[int]:
+    """Return list of integers which are adjacent to a symbol."""
+    clean_list = []
+    for i, check in zip(integers_list, symbols_check):
+        clean_list.append(i) if check else None
+    return clean_list
 
 
 if __name__ == "__main__":
