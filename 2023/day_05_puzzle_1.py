6e24bedb6f9c9591a153d8fd3aeb5222f97a99d6
Maximilian Springmann <maximilian.springmann@gfk.com>
2023-12-07 08:16:12 +0100

    add solution for puzzle 1, day 5

--- 2023/day_05_puzzle_1.py
+++ 2023/day_05_puzzle_1.py
@@ -1,7 +1,7 @@
 """Advent of code, 05.12.2023, puzzle 1"""
 
 import os
-
+from typing import Callable
 from input_handling import read_input_file
 
 example_input = [
@@ -40,20 +40,33 @@ example_input = [
     "56 93 4",
 ]
 
+example_output = [
+    [79, 81, 81, 81, 74, 78, 78, 82],
+    [14, 14, 53, 49, 42, 42, 43, 43],
+    [55, 57, 57, 53, 46, 82, 82, 86],
+    [13, 13, 52, 41, 34, 34, 35, 35],
+]
+example_min_location = 35
 input_file = os.path.join("2023", "input", "day_05_input.txt")
 
-seed_to_soil_dict = {}
-soil_to_fertilizer_dict = {}
-fertilizer_to_water_dict = {}
-water_to_light_dict = {}
-light_to_temperature_dict = {}
-temperature_to_humidity_dict = {}
-humidity_to_location_dict = {}
+mapping_order = [
+    "seed-to-soil map",
+    "soil-to-fertilizer map",
+    "fertilizer-to-water map",
+    "water-to-light map",
+    "light-to-temperature map",
+    "temperature-to-humidity map",
+    "humidity-to-location map",
+]
 
 
-def prepare_input(raw_input: list) -> list:
+def prepare_seed_line(seed_line: str) -> list:
+    """Prepare the seed line for further processing."""
+    return [int(i) for i in seed_line.split(":")[1].strip().split(" ")]
+
+
+def prepare_input(raw_input: list, seed_preparation: Callable) -> dict:
     """Prepare the input string for further processing."""
-    list_of_dicts = []
     dict_of_mapping_dicts = {}
     prep_dict = {}
     prep_dict_name = ""
@@ -62,31 +75,62 @@ def prepare_input(raw_input: list) -> list:
             continue
         elif ":" in line:
             if "seeds:" in line:  # information for seeds in one single line
-                dict_of_mapping_dicts["seeds"] = [
-                    int(i) for i in line.split(":")[1].strip().split(" ")
-                ]
+                dict_of_mapping_dicts["seeds"] = seed_preparation(line)
             elif not prep_dict and not dict_of_mapping_dicts:  # skip line
                 continue
             else:
                 if prep_dict_name:  # do not add first empty line
-                    list_of_dicts.append(prep_dict)
                     dict_of_mapping_dicts[prep_dict_name] = prep_dict
                 prep_dict = {}
                 prep_dict_name = line.split(":")[0]
         else:
-            print(line)
-            print(line.split(" "))
             destination_start, source_start, length = line.split(" ")
             min_source, max_source = int(source_start), int(source_start) + int(length)
             min_destination, max_destination = int(destination_start), int(
                 destination_start
             ) + int(length)
             prep_dict[(min_source, max_source)] = (min_destination, max_destination)
-    list_of_dicts.append(prep_dict)
-    # print(list_of_dicts)
-    print(dict_of_mapping_dicts)
+    dict_of_mapping_dicts[prep_dict_name] = prep_dict
+    return dict_of_mapping_dicts
+
+
+def get_destination_for_source(source: int, mapping: dict) -> int:
+    """Get the destination for a given source."""
+    for source_range, destination_range in mapping.items():
+        if source in range(source_range[0], source_range[1]):
+            return destination_range[0] + (source - source_range[0])
+    return source
+
+
+def get_mapping_for_seed(seed: int, mapping: dict):
+    """Get the location for a given seed."""
+    mapping_list = [seed]
+    for map_order in mapping_order:
+        mapping_list.append(
+            get_destination_for_source(mapping_list[-1], mapping.get(map_order))
+        )
+    return mapping_list
+
+
+def get_mapping_for_all_seeds(seeds: list, mapping: dict):
+    """Get the location for all given seeds."""
+    mapping_list = []
+    for seed in seeds:
+        mapping_list.append(get_mapping_for_seed(seed, mapping))
+    return mapping_list
+
+
+def get_minimum_location(mapping_list: list) -> int:
+    """Get the minimum location for a given mapping list."""
+    return min([i[-1] for i in mapping_list])
 
 
 if __name__ == "__main__":
-    prepare_input(example_input)
-    prepare_input(read_input_file(input_file))
+    mapping_dict = prepare_input(example_input, prepare_seed_line)
+    mapping_list = get_mapping_for_all_seeds(mapping_dict.get("seeds"), mapping_dict)
+    assert mapping_list == example_output
+    assert get_minimum_location(mapping_list) == example_min_location
+    mapping_dict = prepare_input(read_input_file(input_file), prepare_seed_line)
+    mapping_list = get_mapping_for_all_seeds(mapping_dict.get("seeds"), mapping_dict)
+    solution = get_minimum_location(mapping_list)
+    print(f"The solution to the puzzle is {solution}.")
